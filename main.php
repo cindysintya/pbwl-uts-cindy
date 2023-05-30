f2c590e0bd9bdccc71472fd6abd0d99dd9430054
cindy sintya <cindysintya2402@gmail.com>
2023-05-30 16:55:20 +0700

    first

---
+++ main.php
@@ -0,0 +1,4 @@
+<h2>Home</h2>
+
+<div class="info">Selamat datang, <strong>Cindy Sintya</strong></div>
+<div>Dimsum adalah makanan ringan yang jadi bagian penting dari kuliner Tiongkok. Jenisnya sangat beragam, karena semua kue dan makanan pembuka bisa dikategorikan sebagai dimsum. Biasanya dim sum dimakan sebagai sarapan atau sarsiimsum adalah makanan ringan yang jadi bagian penting dari kuliner Tiongkok. Jenisnya sangat beragam, karena semua kue dan makanan pembuka bisa dikategorikan sebagai dimsum. Biasanya dim sum dimakan sebagai sarapan atau sarsi.Jadi pada toko dimsaum, kami menjual berbagai dimsam frozen maupun yang matang. </div>
